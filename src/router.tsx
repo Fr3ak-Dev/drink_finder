8872d373140ce540407a088f42322d0d772325b1
gustavo <kg.2k.pl@gmail.com>
2025-05-20 21:53:15 -0400

    feat: implement routing

---
+++ src/router.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import IndexPage from './views/IndexPage';
+
+export default function AppRouter() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<IndexPage />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
