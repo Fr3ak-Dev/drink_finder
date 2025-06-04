fda1069237831ba930d6c7d7f9436026f53e623d
gustavo <kg.2k.pl@gmail.com>
2025-06-03 22:30:57 -0400

    feat: add notification slice to manage notification state in the app store

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -2,8 +2,10 @@ import { create } from "zustand";
 import { createRecipeSlice, type RecipesSliceType } from "./recipeSlice";
 import { devtools } from "zustand/middleware";
 import { createFavoritesSlice, type FavoritesSliceType } from "./favoritesSlice";
+import { createNotificationSlice, type NotificationSliceType } from "./notificationSlice";
 
-export const useAppStore = create<RecipesSliceType & FavoritesSliceType>()(devtools((...a) => ({
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType & NotificationSliceType>()(devtools((...a) => ({
     ...createRecipeSlice(...a),
     ...createFavoritesSlice(...a),
+    ...createNotificationSlice(...a),
 })))
