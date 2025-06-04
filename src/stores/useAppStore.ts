f58a48d3d58ae841b272ba65b1be70ade050c6d7
gustavo <kg.2k.pl@gmail.com>
2025-06-04 00:29:31 -0400

    feat: add AISlice

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -3,9 +3,11 @@ import { createRecipeSlice, type RecipesSliceType } from "./recipeSlice";
 import { devtools } from "zustand/middleware";
 import { createFavoritesSlice, type FavoritesSliceType } from "./favoritesSlice";
 import { createNotificationSlice, type NotificationSliceType } from "./notificationSlice";
+import { createAISlice, type AISlice } from "./aiSlice";
 
-export const useAppStore = create<RecipesSliceType & FavoritesSliceType & NotificationSliceType>()(devtools((...a) => ({
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType & NotificationSliceType & AISlice>()(devtools((...a) => ({
     ...createRecipeSlice(...a),
     ...createFavoritesSlice(...a),
     ...createNotificationSlice(...a),
+    ...createAISlice(...a),
 })))
