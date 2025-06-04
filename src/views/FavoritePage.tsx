7c5bb3b64d2d1dc74820bbffbe406c4e9a372291
gustavo <kg.2k.pl@gmail.com>
2025-06-03 22:25:31 -0400

    feat: enhance FavoritePage layout and display logic for favorite drinks

--- src/views/FavoritePage.tsx
+++ src/views/FavoritePage.tsx
@@ -1,7 +1,28 @@
+import { useMemo } from "react"
+import DrinkCard from "../components/DrinkCard"
+import { useAppStore } from "../stores/useAppStore"
+
 export default function FavoritePage() {
+
+  const favorites = useAppStore((state) => state.favorites)
+  const hasFavorites = useMemo(() => favorites.length, [favorites])
+
   return (
     <>
-        <h1>Favorites</h1>
+      <h1 className="text-6xl font-extrabold">Favorites</h1>
+      {hasFavorites ? (
+        <div className="grid grid-cols-1 md:grid-cols-3 2xl:grid-cols-3 my-10 gap-10">
+          {favorites.map((drink) => (
+            <DrinkCard
+              key={drink.idDrink}
+              drink={drink}
+            />
+          ))}
+        </div>
+      ) : (
+        <p className="my-10 text-center text-2xl">
+          Your favorites will be displayed here.</p>
+      )}
     </>
   )
 }
