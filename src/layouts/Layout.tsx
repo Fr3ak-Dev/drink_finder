ccbcc6ca010edc0c73abc6460b5942b0f9f27354
gustavo <kg.2k.pl@gmail.com>
2025-06-03 22:41:02 -0400

    feat: add Notification component and integrate it into Layout

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import Modal from "../components/Modal";
 import { useAppStore } from "../stores/useAppStore";
 import { useEffect } from "react";
+import Notification from "../components/Notification";
 
 export default function Layout() {
 
@@ -18,6 +19,7 @@ export default function Layout() {
                 <Outlet />
             </main>
             <Modal />
+            <Notification />
         </>
     )
 }
