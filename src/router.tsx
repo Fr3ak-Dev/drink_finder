0347de624f663e15f886099a04a5093edb21369a
gustavo <kg.2k.pl@gmail.com>
2025-06-04 00:07:01 -0400

    feat: lazy load IndexPage and FavoritePage components in AppRouter

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,29 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import IndexPage from './views/IndexPage';
 import Layout from './layouts/Layout';
-import FavoritePage from './views/FavoritePage';
+// import IndexPage from './views/IndexPage';
+// import FavoritePage from './views/FavoritePage';
+import { lazy, Suspense } from 'react';
+
+const IndexPage = lazy(() => import('./views/IndexPage'));
+const FavoritePage = lazy(() => import('./views/FavoritePage'));
 
 export default function AppRouter() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route element={<Layout />}>
-                    <Route path="/" element={<IndexPage />} index />
-                    <Route path="/favorites" element={<FavoritePage />} index />
+                    {/* <Route path="/" element={<IndexPage />} index />
+                    <Route path="/favorites" element={<FavoritePage />} index /> */}
+                    <Route path="/" element={
+                        <Suspense fallback='Loading...'>
+                            <IndexPage />
+                        </Suspense>
+                    } index />
+                    <Route path="/favorites" element={
+                        <Suspense fallback='Loading...'>
+                            <FavoritePage />
+                        </Suspense>
+                    } />
                 </Route>
             </Routes>
         </BrowserRouter>
