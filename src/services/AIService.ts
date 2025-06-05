c16a68e56cda9ddd36ed56537f5468e74500b8c5
gustavo <kg.2k.pl@gmail.com>
2025-06-04 21:43:01 -0400

    feat: update OpenRouter key, enhance AIService, and add loading animation in GenerateAI component

--- src/services/AIService.ts
+++ src/services/AIService.ts
@@ -5,6 +5,8 @@ export default {
     async generateRecipe(prompt: string) {
         const result = streamText({
             model: openrouter('meta-llama/llama-3.3-70b-instruct:free'),
+            // model: openrouter('deepseek/deepseek-chat-v3-0324:free'),
+            // model: openrouter('google/gemma-3-4b-it:free'),
             prompt
         })
         return result.textStream
